8f4a61403f9fa42d5e0ac5d2de326e3905158739
Oisin <C17343216@mydit.ie>
2020-10-21 19:11:21 +0100

    CarDemo

---
+++ Chaos Riders/Assets/Oisin/Scripts/findcenter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class findcenter : MonoBehaviour
+{
+
+    public GameObject o;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Debug.Log( GetComponent<Renderer>().bounds.center);
+        o.transform.position = GetComponent<Renderer>().bounds.center;
+    }
+}
---
+++ Chaos Riders/Assets/Oisin/Scripts/shootrotatedemo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootrotatedemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("ControllerVert") * speed, 0);
+    }
+}
