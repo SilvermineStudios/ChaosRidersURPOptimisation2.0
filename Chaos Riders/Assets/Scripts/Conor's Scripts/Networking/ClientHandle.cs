240e990e42e27954fbd183a6021a76125cddcea4
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-10-21 18:57:59 +0100

    Can have a player in the editor and in a build

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/ClientHandle.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/ClientHandle.cs
@@ -17,11 +17,13 @@ public class ClientHandle : MonoBehaviour
         Client.instance.udp.Connect(((IPEndPoint)Client.instance.tcp.socket.Client.LocalEndPoint).Port);
     }
 
-    public static void UDPTest(Packet _packet)
+    public static void SpawnPlayer(Packet _packet)
     {
-        string _msg = _packet.ReadString();
+        int _id = _packet.ReadInt();
+        string _username = _packet.ReadString();
+        Vector3 _position = _packet.ReadVector3();
+        Quaternion _rotation = _packet.ReadQuaternion();
 
-        Debug.Log($"Recieved packet via UDP. Contains message: {_msg}");
-        ClientSend.UDPTestRecieved();
+        GameManager.instance.SpawnPlayer(_id, _username, _position, _rotation);
     }
 }
--- Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/ClientSend.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/ClientSend.cs
@@ -27,15 +27,5 @@ public class ClientSend : MonoBehaviour
             SendTCPData(_packet);
         }
     }
-
-    public static void UDPTestRecieved()
-    {
-        using (Packet _packet = new Packet((int)ClientPackets.udpTestRecieved))
-        {
-            _packet.Write("Recieved a UDP packet.");
-
-            SendUDPData(_packet);
-        }
-    }
     #endregion
 }
