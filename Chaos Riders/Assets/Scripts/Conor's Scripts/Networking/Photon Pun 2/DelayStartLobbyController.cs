92b9b182449d4393a45756ede38043ceb11232e3
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-10-22 17:46:52 +0100

    Waiting Room working

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/DelayStartLobbyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class DelayStartLobbyController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject delayStartButton; //button used for creating and joining a game.
+    [SerializeField]
+    private GameObject delayCancelButton; //button used to stop searching for a game to join.
+    [SerializeField]
+    private int RoomSize; //Manual set the number of players in the room at one time.
+
+    public override void OnConnectedToMaster()//callback function for when the first connection is established
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        delayStartButton.SetActive(true);
+    }
+
+    #region Delay Start Button
+    public void DelayStartButton() //funciton for the quickstart button
+    {
+        delayStartButton.SetActive(false);
+        delayCancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom(); //First trys to join an existing room
+        Debug.Log("Quick Start");
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message) //function that plays when join random fails
+    {
+        Debug.Log("Failed to join a room");
+        CreateRoom();
+    }
+
+    void CreateRoom() //trying to create our own room
+    {
+        Debug.Log("Creating room now");
+        int randomRoomNumber = Random.Range(0, 10000); //creating a random name for the room
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)RoomSize }; //requirements for the room
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps); //attempting to create a new room
+        Debug.Log("Random room number: " + randomRoomNumber);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) //function that plays when creating a room fails
+    {
+        Debug.Log("Failed to create room... trying again");
+        CreateRoom(); //trying to create a different room with a new name
+    }
+    #endregion
+
+    public void DelayCancelButton() //function for the cancel search button
+    {
+        delayCancelButton.SetActive(false);
+        delayStartButton.SetActive(true);
+        PhotonNetwork.LeaveRoom(); //leave the room you were searching for
+    }
+}
---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/DelayStartRoomController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class DelayStartRoomController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int waitingRoomSceneIndex; // number for the build index to the multiplayer scene
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom() //function that plays when you successfully create or join a room
+    {
+        Debug.Log("Joined Room");
+        SceneManager.LoadScene(waitingRoomSceneIndex);
+    }
+}
