39c2fc60c6e39a72d1cf7a68d3a1ee44a2deaad0
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-10-22 01:56:31 +0100

    imported Pun 2

--- Chaos Riders/Assets/Imported/Invector-3rdPersonController_LITE/Scripts/CharacterController/vThirdPersonController.cs
+++ Chaos Riders/Assets/Imported/Invector-3rdPersonController_LITE/Scripts/CharacterController/vThirdPersonController.cs
@@ -124,5 +124,6 @@ namespace Invector.vCharacterController
             else
                 animator.CrossFadeInFixedTime("JumpMove", .2f);
         }
+   
     }
 }
---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/NetworkController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    //https://doc-api.photonengine.com/en/pun/v2/index.html
+    //https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); // connects to photon master servers
+    }
+
+    //callback function for when the first connection is established
+    public override void OnConnectedToMaster() 
+    {
+        Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server"); //outputs the connected region to the console
+    }
+
+    void Update()
+    {
+        
+    }
+}
