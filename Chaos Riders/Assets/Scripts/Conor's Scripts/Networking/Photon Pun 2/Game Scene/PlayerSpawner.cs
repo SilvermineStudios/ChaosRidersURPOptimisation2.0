fffc68f8405080cc29f85fb21faee1ac7a4bdec4
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-10-25 15:38:13 +0000

    bla

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/Game Scene/PlayerSpawner.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/Game Scene/PlayerSpawner.cs
@@ -10,16 +10,38 @@ public class PlayerSpawner : MonoBehaviour
     int numberPlayers;
     [SerializeField] private int maxPlayers;
 
+    public int spawnTimer = 5;
+
+    public GameObject playerCar;
+
+    private PhotonPlayer[] photonPlayers;
+    public GameObject[] photonPlayersGO;
+
     private void Awake()
     {
+        photonPlayers = new PhotonPlayer[maxPlayers];
+        photonPlayersGO = new GameObject[maxPlayers];
         CheckPlayers();
+    }
+
+    private void Start()
+    {
         SpawnPlayers();
+    }
+
+    void Update()
+    {
+        photonPlayersGO = (GameObject[])GameObject.FindGameObjectsWithTag("PhotonPlayer");
+        photonPlayers = GetComponents<PhotonPlayer>();
+
+        UpdatePlayerIndex();
     }
 
     void CheckPlayers()
     {
         numberPlayers = PhotonNetwork.CountOfPlayers;
 
+        /*
         for (int i = 0; i <= numberPlayers; i++)
         {
             if (numberPlayers > maxPlayers)
@@ -27,13 +49,41 @@ public class PlayerSpawner : MonoBehaviour
                 numberPlayers -= maxPlayers;
             }
         }
+        */
     }
 
+    
     void SpawnPlayers()
     {
-        for(int i = 0; i <= numberPlayers; i++)
+        //playerCar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CarAvatar"), GameSetup.gs.spawnPoints[1].position, GameSetup.gs.spawnPoints[1].rotation, 0);
+
+        for (int i = 0; i <= PhotonNetwork.CountOfPlayers - 1; i++)
         {
-            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CarAvatar"), GameSetup.gs.spawnPoints[i].position, GameSetup.gs.spawnPoints[i].rotation, 0);
+            playerCar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CarAvatar"), GameSetup.gs.spawnPoints[i].position, GameSetup.gs.spawnPoints[i].rotation, 0);
+            //playerCar.transform.SetParent(photonPlayersGO[i].transform, false);
         }
+    }
+    
+
+    void UpdatePlayerIndex()
+    {
+        for (int i = 0; i <= numberPlayers - 1; i++)
+        {
+            photonPlayersGO[i].GetComponent<PhotonPlayer>().arrayIndex = i;
+        }
+    }
+
+    private IEnumerator Timer(float time, int i)
+    {
+        Debug.Log("Timer started");
+
+        yield return new WaitForSeconds(time);
+
+        Debug.Log("Timer done");
+
+        //if (pv.IsMine)
+        //{
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CarAvatar"), GameSetup.gs.spawnPoints[i].position, GameSetup.gs.spawnPoints[i].rotation, 0);
+        //}
     }
 }
--- Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/GameControllers/PhotonPlayer.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/GameControllers/PhotonPlayer.cs
@@ -7,26 +7,53 @@ using System.IO;
 
 public class PhotonPlayer : MonoBehaviour
 {
-    private PhotonView pv;
+    public int arrayIndex;
+    private float time;
+    private PlayerSpawner playerS;
+
+    public PhotonView pv;
     public GameObject myAvatar;
+
+    private void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
 
     void Start()
     {
-        pv = GetComponent<PhotonView>();
+        playerS = FindObjectOfType<PlayerSpawner>();
+        time = playerS.spawnTimer;
+        
         int spawnPicker = Random.Range(0, GameSetup.gs.spawnPoints.Length); //select a random spawn point out of the list of spawn points on the gamesetup script
 
         //Debug.Log("Photon View Name" + pv.ViewID);
 
         if(pv.IsMine)
         {
-            //myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"), GameSetup.gs.spawnPoints[spawnPicker].position, GameSetup.gs.spawnPoints[spawnPicker].rotation, 0);
             //myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CarAvatar"), GameSetup.gs.spawnPoints[spawnPicker].position, GameSetup.gs.spawnPoints[spawnPicker].rotation, 0);
+            //Debug.Log("Player: " + arrayIndex + " spawned at spawnpoint: " + arrayIndex);
+            //myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CarAvatar"), GameSetup.gs.spawnPoints[arrayIndex].position, GameSetup.gs.spawnPoints[arrayIndex].rotation, 0);
+        }
+        //StartCoroutine(Timer(time));
+    }
+
+    private IEnumerator Timer(float time)
+    {
+        Debug.Log("Timer started");
+
+        yield return new WaitForSeconds(time);
+
+        Debug.Log("Timer done");
+
+        if (pv.IsMine)
+        {
+            //myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CarAvatar"), GameSetup.gs.spawnPoints[arrayIndex].position, GameSetup.gs.spawnPoints[arrayIndex].rotation, 0);
         }
     }
 
-    
-    void Update()
+    [PunRPC]
+    void RPC_GetIndex()
     {
-        
+
     }
 }
