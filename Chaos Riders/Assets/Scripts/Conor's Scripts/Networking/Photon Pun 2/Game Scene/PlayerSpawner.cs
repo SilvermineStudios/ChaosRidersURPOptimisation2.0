7724c465c2658444c885001b0fe5d9ebed38ded2
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-10-27 00:22:57 +0000

    only host has start button

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/Game Scene/PlayerSpawner.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/Game Scene/PlayerSpawner.cs
@@ -8,77 +8,24 @@ using UnityEngine.UI;
 
 public class PlayerSpawner : MonoBehaviourPunCallbacks
 {
-    public Button startButton;
-    int numberPlayers;
-    [SerializeField] private int maxPlayers;
+    private Button startButton;
+    public GameObject startButtonGO;
 
     public int spawnTimer = 5;
 
     public GameObject playerCar;
 
-    private PhotonPlayer[] photonPlayers;
-    public GameObject[] photonPlayersGO;
-    private PhotonView pv;
-
     private void Awake()
     {
-        photonPlayers = new PhotonPlayer[maxPlayers];
-        photonPlayersGO = new GameObject[maxPlayers];
-        pv = GetComponent<PhotonView>();
-        CheckPlayers();
+        startButton = startButtonGO.GetComponent<Button>();
         Button();
     }
 
     private void Start()
     {
-        //SpawnPlayers();
-
         startButton.onClick.AddListener(StartGame);
-        //startButton.interactable = false;
     }
 
-    void Update()
-    {
-        photonPlayersGO = (GameObject[])GameObject.FindGameObjectsWithTag("PhotonPlayer");
-        photonPlayers = GetComponents<PhotonPlayer>();
-
-        UpdatePlayerIndex();
-
-        
-    }
-
-    void CheckPlayers()
-    {
-        numberPlayers = PhotonNetwork.CountOfPlayers;
-
-        /*
-        for (int i = 0; i <= numberPlayers; i++)
-        {
-            if (numberPlayers > maxPlayers)
-            {
-                numberPlayers -= maxPlayers;
-            }
-        }
-        */
-    }
-
-    
-    void SpawnPlayers()
-    {
-        for (int i = 0; i <= PhotonNetwork.CountOfPlayers - 1; i++)
-        {
-            //playerCar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CarAvatar"), GameSetup.gs.spawnPoints[i].position, GameSetup.gs.spawnPoints[i].rotation, 0);
-        }
-    }
-    
-
-    void UpdatePlayerIndex()
-    {
-        for (int i = 0; i <= numberPlayers - 1; i++)
-        {
-            //photonPlayersGO[i].GetComponent<PhotonPlayer>().arrayIndex = i;
-        }
-    }
 
     private IEnumerator Timer(float time, int i)
     {
@@ -86,26 +33,17 @@ public class PlayerSpawner : MonoBehaviourPunCallbacks
 
         yield return new WaitForSeconds(time);
 
-        Debug.Log("Timer done");
-
-        //if (pv.IsMine)
-        //{
-            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CarAvatar"), GameSetup.gs.spawnPoints[i].position, GameSetup.gs.spawnPoints[i].rotation, 0);
-        //}
+        //StartGame();
     }
 
     void StartGame()
     {
+        startButtonGO.SetActive(false);
+
         for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
         {
             photonView.RPC("RPC_StartGame", PhotonNetwork.PlayerList[i], GameSetup.gs.spawnPoints[i].position, GameSetup.gs.spawnPoints[i].rotation);
         }
-        /*
-        for (int i = 0; i < photonPlayers.Length; i++)
-        {
-            photonView.RPC("RPC_StartGame", PhotonNetwork.PlayerList[i], GameSetup.gs.spawnPoints[i].position, GameSetup.gs.spawnPoints[i].rotation);
-        }
-        */
     }
 
     [PunRPC]
@@ -117,14 +55,8 @@ public class PlayerSpawner : MonoBehaviourPunCallbacks
     private void Button()
     {
         if (PhotonNetwork.IsMasterClient)
-        {
-            startButton.interactable = true;
-            //startButton.GetComponent<GameObject>().SetActive(true);
-        }
+            startButtonGO.SetActive(true);
         else
-        {
-            startButton.interactable = false;
-            //startButton.GetComponent<GameObject>().SetActive(false);
-        }
+            startButtonGO.SetActive(false);
     }
 }
