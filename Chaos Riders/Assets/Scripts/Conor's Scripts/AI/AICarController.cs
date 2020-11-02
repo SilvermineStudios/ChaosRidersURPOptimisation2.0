5a2e5f33fdd4f09e90964cb5630d27fb92acf552
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-11-02 14:57:03 +0000

    waypoints accecable through ai car and waypoint manager

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/AI/AICarController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICarController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float distanceFromNextWaypoint;
+
+    [SerializeField] private WaypointManager waypointManager;
+    [SerializeField] private Transform[] waypoints;
+
+    private void Awake()
+    {
+        waypointManager = FindObjectOfType<WaypointManager>(); //find the waypoint manager
+    }
+
+    void Start()
+    {
+        waypoints = new Transform[waypointManager.waypoints.Length];
+        waypoints = waypointManager.waypoints;
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Chaos Riders/Assets/Scripts/Conor's Scripts/AI/WaypointManager.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/AI/WaypointManager.cs
@@ -6,18 +6,12 @@ public class WaypointManager : MonoBehaviour
 {
     public Transform[] waypoints;
 
-    void Start()
+    private void Awake()
     {
         waypoints = new Transform[this.transform.childCount]; //make the array the same length as the amount of children waypoints
         for (int i = 0; i < this.transform.childCount; i++) //put every waypoint(Child) in the array
         {
             waypoints[i] = transform.GetChild(i);
         }
-    }
-
-   
-    void Update()
-    {
-        
     }
 }
