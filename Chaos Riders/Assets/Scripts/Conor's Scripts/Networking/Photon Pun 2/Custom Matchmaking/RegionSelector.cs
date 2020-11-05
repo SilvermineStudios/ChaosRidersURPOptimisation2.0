d8ec86725265327edb9662ffba5a5d826adcba64
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-11-05 14:18:48 +0000

    Region selector working

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/Custom Matchmaking/RegionSelector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+
+public class RegionSelector : MonoBehaviour
+{
+    public TMP_Dropdown regionDropdown;
+
+    private void Start()
+    {
+        HandleInput(0);
+
+        PhotonNetwork.NetworkingClient.AppId = PhotonNetwork.PhotonServerSettings.AppSettings.AppIdRealtime;
+    }
+
+    public void HandleInput(int val)
+    {
+        val = regionDropdown.value;
+
+        if (val == 0)
+            ConnectToEurope();
+
+        if (val == 1)
+            ConnectToRussia();
+    }
+
+
+    
+
+    public void ConnectToEurope()
+    {
+        PhotonNetwork.Disconnect();
+        PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = "eu";
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("EUROPE");
+    }
+
+    public void ConnectToRussia()
+    {
+        PhotonNetwork.Disconnect();
+        PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = "ru";
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("RUSSIA");
+    }
+}
--- Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/NetworkController.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/NetworkController.cs
@@ -10,10 +10,14 @@ public class NetworkController : MonoBehaviourPunCallbacks
     //https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro
     [SerializeField] private TMP_Text servertext; //display for the name of the server you connect to
 
+    [SerializeField] private bool useRegionSelector = false; //choose if you want to manually select your region or not
+
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings(); // connects to photon master servers
+        //IF YOU ARE NOT USING THE REGION SELECTOR AND WANT TO AUTOCONNECT TO NEAREST SERVER
+        if(!useRegionSelector)
+            PhotonNetwork.ConnectUsingSettings(); // connects to photon master servers
     }
 
 
@@ -25,6 +29,6 @@ public class NetworkController : MonoBehaviourPunCallbacks
 
     void Update()
     {
-        
+
     }
 }
