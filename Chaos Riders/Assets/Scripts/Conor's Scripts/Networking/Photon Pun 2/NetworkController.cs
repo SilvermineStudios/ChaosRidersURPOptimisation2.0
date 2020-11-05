ca01767e4073aefd81c8827b3a1659fd8d07d170
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-11-05 11:23:12 +0000

    Server text test

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/NetworkController.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Photon Pun 2/NetworkController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class NetworkController : MonoBehaviourPunCallbacks
 {
     //https://doc-api.photonengine.com/en/pun/v2/index.html
     //https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro
+    [SerializeField] private TMP_Text servertext; //display for the name of the server you connect to
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +20,7 @@ public class NetworkController : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster() //callback function for when the first connection is established
     {
         Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server"); //outputs the connected region to the console
+        servertext.text = "You are connected to the: " + PhotonNetwork.CloudRegion + " server";
     }
 
     void Update()
