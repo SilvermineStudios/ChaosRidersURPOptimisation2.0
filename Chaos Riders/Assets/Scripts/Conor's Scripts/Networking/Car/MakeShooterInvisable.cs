c4d91ec2f6a2a193082752fc5270d040e7ea70de
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-11-08 16:24:18 +0000

    Smooth turret position for car

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Car/MakeShooterInvisable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MakeShooterInvisable : MonoBehaviour
+{
+    //makes the turret invisable to all other players
+    private PhotonView pv;
+    [SerializeField] private MeshRenderer ammoMesh, barrelMesh, standMesh, platformMesh;
+
+
+    void Start()
+    {
+        pv = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!pv.IsMine)
+        {
+            ammoMesh.enabled = false;
+            barrelMesh.enabled = false;
+            standMesh.enabled = false;
+            platformMesh.enabled = false;
+        }
+    }
+}
---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Turret/MakeCarTurretInvisable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MakeCarTurretInvisable : MonoBehaviour
+{
+    private MoveTurretPosition mtp;
+    private GameObject car;
+    private Transform carTurret;
+
+    private MeshRenderer[] meshRenderers;
+    private int gunMeshCount = 4; //ammo, barrel, stand, platform
+
+    private PhotonView pv;
+
+    void Start()
+    {
+        mtp = GetComponent<MoveTurretPosition>();
+        meshRenderers = new MeshRenderer[gunMeshCount];
+        pv = GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        car = mtp.car;
+        carTurret = car.transform.Find("ShooterAttach");
+
+        meshRenderers = carTurret.GetComponentsInChildren<MeshRenderer>();
+
+        if(pv.IsMine)
+        {
+            foreach (MeshRenderer mr in meshRenderers)
+            {
+                mr.enabled = false;
+            }
+        }
+    }
+}
