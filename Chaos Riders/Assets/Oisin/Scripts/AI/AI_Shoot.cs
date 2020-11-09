b0f5fa4121d2b9cafebd809c9c984ab33ae7590d
Oisin <C17343216@mydit.ie>
2020-11-09 14:15:39 +0000

    Aii

---
+++ Chaos Riders/Assets/Oisin/Scripts/AI/AI_Shoot.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Shoot : MonoBehaviour
+{
+
+    public GameObject spawnpoint;
+    public GameObject bullet;
+
+    public float timeSinceLastBullet, fireRate;
+    private AudioSource speaker;
+    public AudioClip gunShot;
+
+    AI_Aiming aiScript;
+
+    void Start()
+    {
+        aiScript = GetComponentInParent<AI_Aiming>();
+        timeSinceLastBullet = fireRate;
+        speaker = GetComponent<AudioSource>();
+    }
+
+
+
+    void Update()
+    {
+        if (aiScript.targetTransform != null)
+        {
+            if (timeSinceLastBullet > fireRate)
+            {
+                fireBullet();
+                timeSinceLastBullet = 0;
+            }
+            else
+            {
+                timeSinceLastBullet += Time.deltaTime;
+            }
+        }
+        else
+        {
+            timeSinceLastBullet = fireRate;
+        }
+    }
+
+
+    void fireBullet()
+    {
+        GameObject fired = Instantiate(bullet, spawnpoint.transform.position, transform.rotation);
+        fired.GetComponent<Rigidbody>().AddForce(transform.forward * 100, ForceMode.Impulse);
+
+        speaker.PlayOneShot(gunShot);
+    }
+}
