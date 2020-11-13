8862eddc35ba90acf8c0a3eafbac9052d3e148da
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-11-13 03:01:00 +0000

    New waypoints

--- Chaos Riders/Assets/Scripts/Conor's Scripts/AI/WaypointManager.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/AI/WaypointManager.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class WaypointManager : MonoBehaviour
 {
+    [SerializeField] private bool drawLines = true;
+    [SerializeField] private bool drawSpheres = true;
     [SerializeField] private Color lineColour;
     [SerializeField] private float sphereRadius = 0.3f;
     public Transform[] waypoints;
+
+
+    [SerializeField] private GameObject waypointPrefab;
+    [SerializeField] private float yOffset = -6f;
+
+    
+
+
+    private void Awake()
+    {
+        AddCarWaypoints();
+    }
 
     private void OnDrawGizmos()
     {
@@ -18,6 +32,11 @@ public class WaypointManager : MonoBehaviour
             waypoints[i] = transform.GetChild(i);
         }
 
+        DrawLinesAndBalls();
+    }
+
+    private void DrawLinesAndBalls()
+    {
         //draw a line between all the waypoints
         for (int i = 0; i < waypoints.Length; i++)
         {
@@ -30,10 +49,29 @@ public class WaypointManager : MonoBehaviour
             else if (i == 0 && waypoints.Length > 1)
                 previosWaypoint = waypoints[waypoints.Length - 1].position;
 
-            Gizmos.DrawLine(previosWaypoint, currentWaypoint);
-            Gizmos.DrawSphere(currentWaypoint, sphereRadius);
+            if (drawLines)
+                Gizmos.DrawLine(previosWaypoint, currentWaypoint);
+            if (drawSpheres)
+                Gizmos.DrawSphere(currentWaypoint, sphereRadius);
         }
     }
+
+    private void AddCarWaypoints()
+    {
+        //spawns the waypoint prefab at each waypoint
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            Vector3 spawnPos = new Vector3(waypoints[i].position.x, waypoints[i].position.y + yOffset, waypoints[i].position.z);  
+            Instantiate(waypointPrefab, spawnPos, waypoints[i].rotation);
+        }
+    }
+
+
+
+
+
+
+
 
 
 
@@ -50,10 +88,4 @@ public class WaypointManager : MonoBehaviour
      * 
      */
     #endregion
-
-
-
-
-
-
 }
