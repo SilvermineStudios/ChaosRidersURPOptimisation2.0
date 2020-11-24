c0de09ed657860cd9fd7130320f9cc17dbc628d8
Tapley123 <conortapley1@gmail.com>
2020-11-24 21:47:23 +0000

    Cooldown bar

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/AbiliyCooldowns.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbiliyCooldowns : MonoBehaviour
+{
+    public Transform GrenadeCooldownBar;
+
+    [SerializeField] private float currentAmount;
+    [SerializeField] private float speed;
+
+
+    void Update()
+    {
+        if(currentAmount < 100)
+        {
+            currentAmount += speed * Time.deltaTime;
+        }
+    }
+}
