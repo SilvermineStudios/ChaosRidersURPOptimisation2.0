1bef6eb4f4d9a8c01329a9cc6ac400353d73e7b2
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-11-25 01:19:19 +0000

    Overcharge bar working

--- Chaos Riders/Assets/Scripts/Conor's Scripts/AbiliyCooldowns.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/AbiliyCooldowns.cs
@@ -5,21 +5,48 @@ using UnityEngine.UI;
 
 public class AbiliyCooldowns : MonoBehaviour
 {
-    public Transform grenadeCooldownBar;
-    public Transform nitroGuzzlerCooldownBar;
+    public Transform grenadeChargeBar, grenadeOverChargeBar;
+    public Transform nitroGuzzlerChargeBar, nitroGuzzlerOverChargeBar;
 
-    [SerializeField] private float currentAmount;
+    [SerializeField] private float standardChargeAmount, overchargeAmount;
     [SerializeField] private float speed;
 
 
+    private void Start()
+    {
+        grenadeChargeBar.GetComponent<Image>().fillAmount = 0;
+        grenadeOverChargeBar.GetComponent<Image>().fillAmount = 0;
+        nitroGuzzlerChargeBar.GetComponent<Image>().fillAmount = 0;
+        nitroGuzzlerOverChargeBar.GetComponent<Image>().fillAmount = 0;
+    }
+
     void Update()
     {
-        if(currentAmount < 100)
+        //if the grenade bar isnt full add to it
+        if (grenadeChargeBar.GetComponent<Image>().fillAmount < 1)
         {
-            currentAmount += speed * Time.deltaTime;
+            standardChargeAmount += speed * Time.deltaTime;
+            grenadeChargeBar.GetComponent<Image>().fillAmount = standardChargeAmount / 100;
+        }
+        //if the grenade bar is full add to the overcharge bar
+        if(grenadeChargeBar.GetComponent<Image>().fillAmount == 1)
+        {
+            overchargeAmount += speed * Time.deltaTime;
+            grenadeOverChargeBar.GetComponent<Image>().fillAmount = overchargeAmount / 100;
         }
 
-        grenadeCooldownBar.GetComponent<Image>().fillAmount = currentAmount / 100;
-        nitroGuzzlerCooldownBar.GetComponent<Image>().fillAmount = currentAmount / 100;
+
+        //if the nitroguzzler bar isnt full add to it
+        if (nitroGuzzlerChargeBar.GetComponent<Image>().fillAmount < 1)
+        {
+            standardChargeAmount += speed * Time.deltaTime;
+            nitroGuzzlerChargeBar.GetComponent<Image>().fillAmount = standardChargeAmount / 100;
+        }
+        //if the nitroguzzler bar is full add to the overcharge bar
+        if (nitroGuzzlerChargeBar.GetComponent<Image>().fillAmount == 1)
+        {
+            overchargeAmount += speed * Time.deltaTime;
+            nitroGuzzlerOverChargeBar.GetComponent<Image>().fillAmount = overchargeAmount / 100;
+        }
     }
 }
