dade8659c3fc85a5b1a532d7d2545b67cde417f9
Tapley123 <conortapley1@gmail.com>
2020-12-06 13:38:33 +0000

    Network sync start

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Driver Abilities/CarUIManager.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Driver Abilities/CarUIManager.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Photon.Pun;
 
 public class CarUIManager : MonoBehaviour
 {
+    private PhotonView pv;
+
     [SerializeField] private GameObject youWinTextGo;
     public static GameObject youWinText;
 
@@ -13,13 +16,28 @@ public class CarUIManager : MonoBehaviour
     [SerializeField] private TMP_Text lapsTextUI;
     public static TMP_Text lapsText;
 
+
+    [SerializeField] private GameObject minimapCam;
+    public static GameObject minimapCamera;
+
     void Awake()
     {
         youWinText = youWinTextGo;
         lapsText = lapsTextUI;
+        minimapCamera = minimapCam;
+        pv = GetComponent<PhotonView>();
     }
 
-    
+    private void Start()
+    {
+        if (pv.IsMine && IsThisMultiplayer.Instance.multiplayer || !IsThisMultiplayer.Instance.multiplayer)
+        {
+            minimapCam.SetActive(true); //activate the minimap camera if it belongs to you
+        } 
+        else
+            minimapCam.SetActive(false); //disable all the minimap cameras that dont belong to you
+    }
+
     void Update()
     {
         
