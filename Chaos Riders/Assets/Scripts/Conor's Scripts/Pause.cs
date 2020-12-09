c1667be90d23ced2903c7838a0d95180710b3264
Tapley123 <conortapley1@gmail.com>
2020-12-09 21:58:07 +0000

    First pause menu

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Pause.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseMenu; //Pause Menu Gameobject 
+    [SerializeField] private KeyCode pauseMenuButton1, pauseMenuButton2;
+    [SerializeField] private bool paused = false;
+    private PhotonView pv;
+
+    void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
+
+    void Start()
+    {
+        if (pv.IsMine && IsThisMultiplayer.Instance.multiplayer || !IsThisMultiplayer.Instance.multiplayer)
+        {
+            PauseMenu.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (pv.IsMine && IsThisMultiplayer.Instance.multiplayer || !IsThisMultiplayer.Instance.multiplayer)
+        {
+            //set paused = true / false
+            if(Input.GetKey(pauseMenuButton1) || Input.GetKey(pauseMenuButton2))
+            {
+                if (paused)
+                    paused = false;
+                if (!paused)
+                    paused = true;
+            }
+
+
+            if (paused)
+                PauseMenu.SetActive(true);
+            else
+                PauseMenu.SetActive(false);
+        }
+    }
+}
