58be018c4d264b0405e774d6096fb1ef8f40c4a4
Tapley123 <conortapley1@gmail.com>
2020-12-15 16:33:53 +0000

    Better Make Children Invisible script

--- Chaos Riders/Assets/Scripts/Conor's Scripts/MakeAllChildrenInvisable.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/MakeAllChildrenInvisable.cs
@@ -2,18 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class MakeAllChildrenInvisable : MonoBehaviour
 {
-    //[SerializeField] private MeshRenderer ammoMesh, barrelMesh, standMesh, platformMesh;
+    [SerializeField] private bool makeInvisable = false;
+
     [SerializeField] private MeshRenderer[] meshRenderers;
 
     // Update is called once per frame
     void Update()
     {
         meshRenderers = this.GetComponentsInChildren<MeshRenderer>();
+
+        if(makeInvisable)
+        {
             foreach (MeshRenderer mr in meshRenderers)
             {
                 mr.enabled = false;
             }
+        }
+        else
+        {
+            foreach (MeshRenderer mr in meshRenderers)
+            {
+                mr.enabled = true;
+            }
+        }
     }
 }
