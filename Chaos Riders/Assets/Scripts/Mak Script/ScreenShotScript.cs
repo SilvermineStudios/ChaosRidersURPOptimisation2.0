1501b15baff85d7ed78e3b54b60acfc2f289689f
IIMakII <37370335+IIMakII@users.noreply.github.com>
2020-12-16 23:31:24 +0000

    Added new lights to desert scene

---
+++ Chaos Riders/Assets/Scripts/Mak Script/ScreenShotScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShotScript : MonoBehaviour
+{
+    private static ScreenShotScript instance;
+
+    private Camera myCamera;
+    private bool takeScreenshotOnNextFrame;
+
+    private void Awake()
+    {
+        instance = this;
+        myCamera = gameObject.GetComponent<Camera>();
+    }
+
+    private void OnPostRender()
+    {
+        if (takeScreenshotOnNextFrame)
+        {
+            takeScreenshotOnNextFrame = false;
+            RenderTexture renderTexture = myCamera.targetTexture;
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes(Application.dataPath + "/CameraScreenshot.png", byteArray);
+        }
+    }
+}
