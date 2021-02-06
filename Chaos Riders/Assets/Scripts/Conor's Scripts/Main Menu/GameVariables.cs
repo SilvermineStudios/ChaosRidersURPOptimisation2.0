5100b74a71b7438e470bb3622742a5d24396697d
Tapley123 <conortapley1@gmail.com>
2021-02-06 20:31:27 +0000

    Laps counter works for host and non hosts

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Main Menu/GameVariables.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Main Menu/GameVariables.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Photon.Pun;
 
 public class GameVariables : MonoBehaviour
 {
+    private PhotonView pv;
+
+    [SerializeField] private GameObject hostScreen, nonHostScreen;
     public TMP_Text nonHostLapText;
 
     public static int Laps;
@@ -25,12 +29,8 @@ public class GameVariables : MonoBehaviour
 
     void Awake()
     {
+        pv = GetComponent<PhotonView>();
         
-    }
-
-    
-    void Update()
-    {
         Laps = laps;
         AmountOfAICars = amountOfAICars;
         NitroPickup = nitroPickup;
@@ -38,11 +38,43 @@ public class GameVariables : MonoBehaviour
         RPGPickup = rpgPickup;
     }
 
+    
+    void Update()
+    {
+        
+
+        if (PhotonNetwork.IsMasterClient) 
+        {
+            hostScreen.SetActive(true);
+            nonHostScreen.SetActive(false);
+        }
+        else
+        {
+            hostScreen.SetActive(false);
+            nonHostScreen.SetActive(true);
+        }
+
+
+        //Laps = laps;
+        AmountOfAICars = amountOfAICars;
+        NitroPickup = nitroPickup;
+        ShieldPickup = shieldPickup;
+        RPGPickup = rpgPickup;
+    }
+
+    //runs everytime the host changes the lap count
     public void HangleLapsInputData(int val)
     {
-        int lapVal = val + 1;
+        pv.RPC("UpdateLapText", RpcTarget.AllBuffered, val);
+    }
 
+    [PunRPC]
+    void UpdateLapText(int val)
+    {
+        int lapVal = val + 1;
         laps = lapVal;
-        nonHostLapText.text = "Laps: " + lapVal.ToString();
+        nonHostLapText.text = "Laps: " + laps;
+        Laps = laps;
+        //Debug.Log("Laps = " + Laps + " laps = " + laps);
     }
 }
