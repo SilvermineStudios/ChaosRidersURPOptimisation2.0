78f606cf9716e24315f03f1855fa93b9521ef42e
Tapley123 <conortapley1@gmail.com>
2021-02-08 00:51:32 +0000

    pregame lobby menu working

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Spawning Players/CharacterScreenChanger.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Spawning Players/CharacterScreenChanger.cs
@@ -66,5 +66,113 @@ public class CharacterScreenChanger : MonoBehaviourPunCallbacks
             shooterCharacterScreen.SetActive(true);
             driverCharacterScreen.SetActive(false);
         }
+        //Player 3
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 3)
+        {
+            shooterCharacterScreen.SetActive(false);
+            driverCharacterScreen.SetActive(true);
+        }
+        //Player 4
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 4)
+        {
+            shooterCharacterScreen.SetActive(true);
+            driverCharacterScreen.SetActive(false);
+        }
+        //Player 5
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 5)
+        {
+            shooterCharacterScreen.SetActive(false);
+            driverCharacterScreen.SetActive(true);
+        }
+        //Player 6
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 6)
+        {
+            shooterCharacterScreen.SetActive(true);
+            driverCharacterScreen.SetActive(false);
+        }
+        //Player 7
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 7)
+        {
+            shooterCharacterScreen.SetActive(false);
+            driverCharacterScreen.SetActive(true);
+        }
+        //Player 8
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 8)
+        {
+            shooterCharacterScreen.SetActive(true);
+            driverCharacterScreen.SetActive(false);
+        }
+        //Player 9
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 9)
+        {
+            shooterCharacterScreen.SetActive(false);
+            driverCharacterScreen.SetActive(true);
+        }
+        //Player 10
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 10)
+        {
+            shooterCharacterScreen.SetActive(true);
+            driverCharacterScreen.SetActive(false);
+        }
+        //Player 11
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 11)
+        {
+            shooterCharacterScreen.SetActive(false);
+            driverCharacterScreen.SetActive(true);
+        }
+        //Player 12
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 12)
+        {
+            shooterCharacterScreen.SetActive(true);
+            driverCharacterScreen.SetActive(false);
+        }
+        //Player 13
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 13)
+        {
+            shooterCharacterScreen.SetActive(false);
+            driverCharacterScreen.SetActive(true);
+        }
+        //Player 14
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 14)
+        {
+            shooterCharacterScreen.SetActive(true);
+            driverCharacterScreen.SetActive(false);
+        }
+        //Player 15
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 15)
+        {
+            shooterCharacterScreen.SetActive(false);
+            driverCharacterScreen.SetActive(true);
+        }
+        //Player 16
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 16)
+        {
+            shooterCharacterScreen.SetActive(true);
+            driverCharacterScreen.SetActive(false);
+        }
+        //Player 17
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 17)
+        {
+            shooterCharacterScreen.SetActive(false);
+            driverCharacterScreen.SetActive(true);
+        }
+        //Player 18
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 18)
+        {
+            shooterCharacterScreen.SetActive(true);
+            driverCharacterScreen.SetActive(false);
+        }
+        //Player 19
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 19)
+        {
+            shooterCharacterScreen.SetActive(false);
+            driverCharacterScreen.SetActive(true);
+        }
+        //Player 20
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 20)
+        {
+            shooterCharacterScreen.SetActive(true);
+            driverCharacterScreen.SetActive(false);
+        }
     }
 }
