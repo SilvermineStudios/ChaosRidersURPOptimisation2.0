1bb62baf9c63701fbbc85e2d32deda82f5fb58ac
Tapley123 <conortapley1@gmail.com>
2021-02-10 13:11:32 +0000

    better ai spawn point system

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Spawning Players/AISpawner.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Spawning Players/AISpawner.cs
@@ -6,12 +6,22 @@ public class AISpawner : MonoBehaviour
 {
     //get the amount of ai cars to spawn from Game Variables script *********
 
+
     public Transform[] spawnPoints;
     public GameObject[] aiCars;
 
     //from the main menu settings
     [SerializeField] private bool spawnAI;
     [SerializeField] private int amountOfAI;
+
+    private void OnDrawGizmos()
+    {
+        spawnPoints = new Transform[this.transform.childCount]; //make the array the same length as the amount of children waypoints
+        for (int i = 0; i < this.transform.childCount; i++) //put every waypoint(Child) in the array
+        {
+            spawnPoints[i] = transform.GetChild(i);
+        }
+    }
 
     private void Awake()
     {
