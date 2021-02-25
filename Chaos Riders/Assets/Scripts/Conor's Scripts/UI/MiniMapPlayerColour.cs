f526e324f9c232e6e140404c3e3435fc241aa6c4
Tapley123 <conortapley1@gmail.com>
2021-02-25 00:20:42 +0000

    Minimap Player icon
    Braker: If the car s yours you are green, if the car is an enemy they are red

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/UI/MiniMapPlayerColour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MiniMapPlayerColour : MonoBehaviour
+{
+    private PhotonView pv;
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        pv = this.transform.parent.gameObject.GetComponent<PhotonView>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        if (pv.IsMine)
+        {
+            sr.color = Color.green;
+        }
+        else
+        {
+            sr.color = Color.red;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
