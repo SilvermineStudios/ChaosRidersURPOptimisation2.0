927d037845b3292af5ec53eabbcfd597c5fb49ca
Tapley123 <conortapley1@gmail.com>
2021-03-05 20:57:46 +0000

    bla

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Player/SmoothServerPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class SmoothServerPosition : MonoBehaviourPun, IPunObservable
+{
+    protected Player Player;
+
+
+    void Awake()
+    {
+        Player = GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        throw new System.NotImplementedException();
+    }
+}
