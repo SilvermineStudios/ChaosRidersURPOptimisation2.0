4da8da0bfea5ba8d469ae98ae3af3d8b6f7b7ff7
Tapley123 <conortapley1@gmail.com>
2021-03-05 21:33:53 +0000

    Update SmoothServerPosition.cs

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Player/SmoothServerPosition.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/Player/SmoothServerPosition.cs
@@ -6,21 +6,45 @@ using Photon.Realtime;
 
 public class SmoothServerPosition : MonoBehaviourPun, IPunObservable
 {
-    protected Player Player;
-
+    protected Controller car;
+    private PhotonView pv;
+    protected Vector3 RemotePlayerPosition;
 
     void Awake()
     {
-        Player = GetComponent<Player>();
+        car = GetComponent<Controller>();
+        pv = GetComponent<PhotonView>();
     }
 
     void Update()
     {
-        
+        if (pv.IsMine)
+            return;
+
+        var LagDistance = RemotePlayerPosition - transform.position;
+
+        //High Distance => sync is to much off => send to position
+        if(LagDistance.magnitude > 5f) //if the player is lagging too much
+        {
+            transform.position = RemotePlayerPosition; //move the car to the remote position
+            LagDistance = Vector3.zero;
+        }
+
+        if(LagDistance.magnitude < 0.11f)
+        {
+            
+        }
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        throw new System.NotImplementedException();
+        if(stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+        }
+        else
+        {
+            RemotePlayerPosition = (Vector3)stream.ReceiveNext();
+        }
     }
 }
