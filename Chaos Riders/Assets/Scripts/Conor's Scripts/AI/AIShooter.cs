fb4945273cf905d95e19a967e6fbfcc2b8b3e873
Tapley123 <conortapley1@gmail.com>
2021-03-19 16:20:23 +0000

    Ai gun is getting the nearest target

--- Chaos Riders/Assets/Scripts/Conor's Scripts/AI/AIShooter.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/AI/AIShooter.cs
@@ -24,17 +24,76 @@ public class AIShooter : MonoBehaviour
     [SerializeField] private Transform impactEffectHolder;
     [SerializeField] GameObject CasingSpawn;
     [SerializeField] GameObject Casing;
+
+    // https://www.youtube.com/watch?v=QKhn2kl9_8I
+    [Header("Brackeys Stuff")]
+    public Transform target;
+    public List<GameObject> targets = new List<GameObject>();
+    public string aiCarTag = "Player";
+    public string carTag = "car";
+    public float range = 30f;
+    [SerializeField] private float timeBeforeLookingForANewTarget = 2f;
+    [SerializeField] private float delayBeforeStartWorking = 3f;
     #endregion
 
-    // Start is called before the first frame update
     void Start()
+    {
+        StartCoroutine(GetTargets(delayBeforeStartWorking));
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateTarget()
     {
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        //cycle through all of the targets in the target array list to find the nearest one
+        foreach(GameObject enemy in targets)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        //if a nearest enemy is found and within range then it becomes the target
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+    }
+
+    //draws a sphere to show the range of the gun
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+    private IEnumerator GetTargets(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        GameObject[] aicars = GameObject.FindGameObjectsWithTag(aiCarTag);
+        GameObject[] cars = GameObject.FindGameObjectsWithTag(carTag);
         
+        foreach(GameObject go in aicars)
+        {
+            targets.Add(go);
+        }
+        foreach(GameObject go in cars)
+        {
+            targets.Add(go);
+        }
+
+        //starts too look for a target
+        InvokeRepeating("UpdateTarget", 0f, timeBeforeLookingForANewTarget); //MOVE TO WHEN WEAPONS ARE ENABLED
     }
 }
