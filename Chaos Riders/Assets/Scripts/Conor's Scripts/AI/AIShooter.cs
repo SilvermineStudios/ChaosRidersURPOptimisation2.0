ea2f078c03844567ed4edcbbe43e822b7b8168cb
Tapley123 <conortapley1@gmail.com>
2021-03-19 15:37:20 +0000

    basic variables on ai shooter

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/AI/AIShooter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIShooter : MonoBehaviour
+{
+    #region Variables
+    [Header("General GameObjects")]
+    public GameObject car;
+    [SerializeField] private Transform minigunBarrel;
+    [SerializeField] private Transform rifleBarrel;
+    [SerializeField] GameObject rpgGo;
+    [SerializeField] GameObject rocketspawn;
+    [SerializeField] GameObject rocket;
+    [SerializeField] private Transform gunBarrel; //barrel that is going to rotate to face the correct direction
+    [SerializeField] private GameObject bulletSpawnPoint;
+    [SerializeField] GameObject MinigunHolder;
+    [SerializeField] GameObject RifleHolder;
+    LayerMask everythingButIgnoreBullets;
+    [SerializeField] public ParticleSystem muzzleFlash;
+
+    [Header ("Bullet Decoration")]
+    public GameObject impactEffect;
+    [SerializeField] private Transform impactEffectHolder;
+    [SerializeField] GameObject CasingSpawn;
+    [SerializeField] GameObject Casing;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
