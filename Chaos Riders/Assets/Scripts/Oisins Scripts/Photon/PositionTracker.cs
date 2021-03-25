dca4f3661ea0701b096e136a264160a8277097df
Oisin <C17343216@mydit.ie>
2021-03-25 11:47:55 +0000

    c

--- Chaos Riders/Assets/Scripts/Oisins Scripts/Photon/PositionTracker.cs
+++ Chaos Riders/Assets/Scripts/Oisins Scripts/Photon/PositionTracker.cs
@@ -1,18 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
 
-public class PositionTracker : MonoBehaviour
+public class PositionTracker : MonoBehaviourPun
 {
-    
-    void Start()
+    PhotonView pv;
+    PlayerDataManager playerDataManager;
+
+    string[] playerNames = new string[20];
+    float[] teamNumbersToPositions = new float[20];
+
+
+
+    List<int> teamNumbers = new List<int>();
+    public string[] playerTeams;
+    public Text[] playerNameTexts;
+    public int[] playerScores;
+    public int scoreTotal;
+    public Text[] playerScoreText;
+    public Transform[] scoreHolder;
+
+
+
+    private void Awake()
     {
-        
+        pv = GetComponent<PhotonView>();
+        playerDataManager = FindObjectOfType<PlayerDataManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    void Start()
     {
-        
+        if (!PhotonNetwork.IsMasterClient) { return; }
+        foreach (PhotonMenuPlayer p in PlayerDataManager.PhotonMenuPlayers)
+        {
+            Debug.Log(p.teamNumber);
+            if(playerNames[p.teamNumber] == null)
+            {
+                playerNames[p.teamNumber] = p.Player.NickName;
+            }
+            else
+            {
+                playerNames[p.teamNumber] = playerNames[p.teamNumber] + " + " + p.Player.NickName;
+            }
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (!PhotonNetwork.IsMasterClient) { return; }
+        foreach (PhotonMenuPlayer p in PlayerDataManager.PhotonMenuPlayers)
+        {
+            if (p.driver)
+            {
+                teamNumbersToPositions[p.teamNumber] = p.myCheckpoint.distanceToNextCheckpoint;
+            }
+        }
+
+
+
     }
 }
