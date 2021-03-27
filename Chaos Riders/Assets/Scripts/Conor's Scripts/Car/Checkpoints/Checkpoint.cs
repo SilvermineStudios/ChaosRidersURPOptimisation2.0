de549a29c1a07cd3c4656030ec7a2db8a82948df
Oisin <C17343216@mydit.ie>
2021-03-27 15:50:02 +0000

    sda

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Car/Checkpoints/Checkpoint.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Car/Checkpoints/Checkpoint.cs
@@ -76,7 +76,7 @@ public class Checkpoint : MonoBehaviour
             }
             if (isResetting && previousCheckpoint != -1)
             {
-                StartCoroutine(UseEquipmentUI(3));
+                StartCoroutine(UseEquipmentUI(1));
             }
             else if (resetBar != null)
             {
@@ -159,13 +159,14 @@ public class Checkpoint : MonoBehaviour
                     currentCheckpoint = 0; //make the next waypoint the first waypoint
                 else
                 {
-                    previousCheckpoint += 1;
-                    if (previousCheckpoint > checkpoints.Length)
-                    {
-                        previousCheckpoint = 0;
-                    }
                     currentCheckpoint += 1;//if the current waypoint is not the last waypoint in the list then go to the next waypoint in the list
+                    previousCheckpoint += 1;
                 }
+                if (currentCheckpoint == 1)
+                {
+                    previousCheckpoint = 0;
+                }
+
             }
 
 
