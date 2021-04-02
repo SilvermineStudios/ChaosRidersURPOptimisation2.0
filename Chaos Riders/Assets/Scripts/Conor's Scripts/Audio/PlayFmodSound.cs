a539303faf35d81e2b239cc7bfaba777895ad517
Tapley123 <conortapley1@gmail.com>
2021-04-02 18:35:09 +0100

    New looping gun sound

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Audio/PlayFmodSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayFmodSound : MonoBehaviour
+{
+    public string EventLocation;
+    FMOD.Studio.EventInstance eventInstance;
+
+    public bool playOnAwake = true;
+
+    void Awake()
+    {
+        eventInstance = FMODUnity.RuntimeManager.CreateInstance(EventLocation);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(eventInstance, this.transform.root.transform, this.transform.root.GetComponent<Rigidbody>());
+
+        if(playOnAwake)
+            eventInstance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!playOnAwake)
+        {
+            
+        }
+    }
+}
