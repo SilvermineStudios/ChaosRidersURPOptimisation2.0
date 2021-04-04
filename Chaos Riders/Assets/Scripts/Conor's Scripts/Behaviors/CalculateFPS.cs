13b932e83aa34211c4f25e998beaac02bf903afa
Tapley123 <conortapley1@gmail.com>
2021-04-04 17:11:33 +0100

    FPS displayed

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Behaviors/CalculateFPS.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Behaviors/CalculateFPS.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CalculateFPS : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text FPSText;
+    private float timer, refresh, avgFramerate;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float timelapse = Time.smoothDeltaTime;
+        timer = timer <= 0 ? refresh : timer -= timelapse;
+
+        if (timer <= 0) avgFramerate = (int) (1f / timelapse);
+        FPSText.text = avgFramerate.ToString();
     }
 }
