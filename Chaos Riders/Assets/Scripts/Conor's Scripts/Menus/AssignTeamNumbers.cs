26188734dc92889ec24eae7cf9082efe0b08e8a9
Oisin <C17343216@mydit.ie>
2021-04-09 14:40:09 +0100

    f

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Menus/AssignTeamNumbers.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Menus/AssignTeamNumbers.cs
@@ -1,25 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun.UtilityScripts;
 public class AssignTeamNumbers : MonoBehaviour
 {
     public void AssignTeamNumber()
     {
         if (PlayerDataManager.Drivers.Count > 0)
         {
-            for (int i = 0; i < PlayerDataManager.Drivers.Count; i++)
+            for (int i = 1; i < PlayerDataManager.Drivers.Count; i++)
             {
                 PlayerDataManager.Drivers[i].GetComponent<PhotonMenuPlayer>().teamNumber = i;
+                PlayerDataManager.Drivers[i].GetComponent<PhotonMenuPlayer>().Player.JoinTeam(1);
+                Debug.Log(PlayerDataManager.Drivers[i].GetComponent<PhotonMenuPlayer>().Player.JoinTeam(1));
+                
                 //Debug.Log(PlayerDataManager.Drivers[i].GetComponent<PhotonMenuPlayer>().Player.NickName + "'s team number is " + PlayerDataManager.Drivers[i].GetComponent<PhotonMenuPlayer>().teamNumber);
             }
         }
 
         if (PlayerDataManager.Shooters.Count > 0)
         {
-            for (int i = 0; i < PlayerDataManager.Shooters.Count; i++)
+            for (int i = 1; i < PlayerDataManager.Shooters.Count; i++)
             {
                 PlayerDataManager.Shooters[i].GetComponent<PhotonMenuPlayer>().teamNumber = i;
+                PlayerDataManager.Drivers[i].GetComponent<PhotonMenuPlayer>().Player.JoinTeam((byte)i);
                 //Debug.Log(PlayerDataManager.Shooters[i].GetComponent<PhotonMenuPlayer>().Player.NickName + "'s team number is " + PlayerDataManager.Shooters[i].GetComponent<PhotonMenuPlayer>().teamNumber);
             }
         }
--- Chaos Riders/Assets/Scripts/Oisins Scripts/Photon/PositionTracker.cs
+++ Chaos Riders/Assets/Scripts/Oisins Scripts/Photon/PositionTracker.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon;
+using Photon.Realtime;
 using UnityEngine.UI;
 
 public class PositionTracker : MonoBehaviourPun
@@ -34,30 +37,46 @@ public class PositionTracker : MonoBehaviourPun
         playerDataManager = FindObjectOfType<PlayerDataManager>();
     }
 
-
+    float f;
 
     void Start()
     {
+        //GetAvailableTeams()
         if (!PhotonNetwork.IsMasterClient) { return; }
         foreach (PhotonMenuPlayer p in PlayerDataManager.PhotonMenuPlayers)
         {
+            {
+                /*
+                if(!teamNumbers.Contains(p.teamNumber))
+                {
+                    teamNumbers.Add(p.teamNumber);
+                }
+                p.driverAndShooterNames = p.Player.NickName;
+                if (p.driver)
+                {
+                    foreach (PhotonMenuPlayer p2 in PlayerDataManager.PhotonMenuPlayers)
+                    {
+                        if(p2.shooter && p.teamNumber == p2.teamNumber)
+                        {
+
+
+                            //p.driverAndShooterNames = p.Player.NickName + " + " + p2.Player.NickName;
+                            //p2.driverAndShooterNames = p.Player.NickName + " + " + p2.Player.NickName;
+
+
+
+                        }
+                    }
+                }*/
+            }
             if(!teamNumbers.Contains(p.teamNumber))
             {
                 teamNumbers.Add(p.teamNumber);
+                
             }
-            p.driverAndShooterNames = p.Player.NickName;
-            if (p.driver)
-            {
-                foreach (PhotonMenuPlayer p2 in PlayerDataManager.PhotonMenuPlayers)
-                {
-                    if(p2.shooter && p.teamNumber == p2.teamNumber)
-                    {
-                        p.driverAndShooterNames = p.Player.NickName + " + " + p2.Player.NickName;
-                        p2.driverAndShooterNames = p.Player.NickName + " + " + p2.Player.NickName;
-
-                    }
-                }
-            }
+            //p.Player.JoinTeam((byte)p.teamNumber);
+            //Debug.Log(p.teamNumber);
+            f = p.teamNumber;
         }
     }
 
@@ -66,14 +85,25 @@ public class PositionTracker : MonoBehaviourPun
     {
         if (!PhotonNetwork.IsMasterClient) { return; }
 
+
+        
+
+
+
+        // Debug.Log(PhotonTeamsManager.Instance.TryGetTeamMembers((byte)1));
+
         foreach (PhotonMenuPlayer p in PlayerDataManager.PhotonMenuPlayers)
         {
-            teamNumbersToPositions[p.teamNumber][0] = p.myCheckpoint.distanceToNextCheckpoint;
+            //teamNumbersToPositions[p.teamNumber][0] = p.myCheckpoint.distanceToNextCheckpoint;
         }
 
         foreach(int i in teamNumbers)
         {
-            Debug.Log(teamNumbersToPositions[i][0]);
+            //Debug.Log(teamNumbersToPositions[i][0]);
         }
+        
+
+
+        //var sortedPlayerList = (from p in playerList orderby p.GetMyScore() descending select p).ToList();
     }
 }
