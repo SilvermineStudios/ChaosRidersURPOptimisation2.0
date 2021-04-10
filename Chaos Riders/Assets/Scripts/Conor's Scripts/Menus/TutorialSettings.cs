a9a64db15b93154a92829816591cf378f0b3f16f
Tapley123 <conortapley1@gmail.com>
2021-04-10 16:48:48 +0100

    most tutorail buttons working

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Menus/TutorialSettings.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Menus/TutorialSettings.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class TutorialSettings : MonoBehaviour
@@ -20,6 +21,16 @@ public class TutorialSettings : MonoBehaviour
     public GameObject driverButtons;
     public GameObject shooterButtons;
     public Color selectedColour;
+    public Color standardColour;
+    public GameObject brakerIcon;
+    public GameObject shredderIcon;
+    public GameObject standarGunIcon;
+    public GameObject goldenGunIcon;
+    public GameObject brakerButton;
+    public GameObject shredderButton;
+    public GameObject standarGunButton;
+    public GameObject goldenGunButton;
+    public GameObject backButton;
 
     void Start()
     {
@@ -36,6 +47,7 @@ public class TutorialSettings : MonoBehaviour
         playerTypeButtons.SetActive(true);
         driverButtons.SetActive(false);
         shooterButtons.SetActive(false);
+        backButton.SetActive(false);
     }
 
     #region Settings
@@ -93,6 +105,7 @@ public class TutorialSettings : MonoBehaviour
         shooterButtons.SetActive(false);
 
         driverButtons.SetActive(true);
+        backButton.SetActive(true);
     }
 
     public void Button_Shooter()
@@ -101,34 +114,70 @@ public class TutorialSettings : MonoBehaviour
         driverButtons.SetActive(false);
 
         shooterButtons.SetActive(true);
+        backButton.SetActive(true);
+    }
+
+    public void Button_CarPicker(int val)
+    {
+        //braker
+        if(val == 0)
+        {
+            brakerIcon.SetActive(true);
+            shredderIcon.SetActive(false);
+            standarGunIcon.SetActive(false);
+            goldenGunIcon.SetActive(false);
+
+            brakerButton.GetComponent<Image>().color = selectedColour;
+            shredderButton.GetComponent<Image>().color = standardColour;
+        }
+
+        //shredder
+        if(val == 1)
+        {
+            brakerIcon.SetActive(false);
+            shredderIcon.SetActive(true);
+            standarGunIcon.SetActive(false);
+            goldenGunIcon.SetActive(false);
+
+            shredderButton.GetComponent<Image>().color = selectedColour;
+            brakerButton.GetComponent<Image>().color = standardColour;
+        }
+    }
+
+    public void Button_ShooterPicker(int val)
+    {
+        //standard gun
+        if(val == 0)
+        {
+            brakerIcon.SetActive(false);
+            shredderIcon.SetActive(false);
+            standarGunIcon.SetActive(true);
+            goldenGunIcon.SetActive(false);
+
+            standarGunButton.GetComponent<Image>().color = selectedColour;
+            goldenGunButton.GetComponent<Image>().color = standardColour;
+        }
+
+        //golden gun
+        if (val == 1)
+        {
+            brakerIcon.SetActive(false);
+            shredderIcon.SetActive(false);
+            standarGunIcon.SetActive(false);
+            goldenGunIcon.SetActive(true);
+
+            goldenGunButton.GetComponent<Image>().color = selectedColour;
+            standarGunButton.GetComponent<Image>().color = standardColour;
+        }
     }
 
     public void Button_Back()
     {
         driverButtons.SetActive(false);
         shooterButtons.SetActive(false);
+        backButton.SetActive(false);
 
         playerTypeButtons.SetActive(true);
-    }
-
-    public void Button_Braker()
-    {
-
-    }
-
-    public void Button_Shredder()
-    {
-
-    }
-
-    public void Button_StandardGun()
-    {
-
-    }
-
-    public void Button_GoldenGun()
-    {
-
     }
     #endregion
 }
