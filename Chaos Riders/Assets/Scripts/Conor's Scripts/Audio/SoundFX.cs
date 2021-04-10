1c82f8d03068965bd34e2a78d88e621543a05d93
Tapley123 <conortapley1@gmail.com>
2021-04-10 15:44:50 +0100

    Better sound effect

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Audio/SoundFX.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Audio/SoundFX.cs
@@ -7,6 +7,8 @@ public class SoundFX : MonoBehaviour
     public string buttonSoundRef = "event:/SoundFX/ButtonSound";
     public string changeSoundEffectVolumeRef = "event:/SoundFX/SoundEffectSound";
 
+    bool canPlaySound = true;
+
     public void PlayButtonSound()
     {
         FMODUnity.RuntimeManager.PlayOneShot(buttonSoundRef, this.transform.position);
@@ -14,6 +16,20 @@ public class SoundFX : MonoBehaviour
 
     public void PlayChangeSoundEffectSound()
     {
-        FMODUnity.RuntimeManager.PlayOneShot(changeSoundEffectVolumeRef, this.transform.position);
+        if(canPlaySound)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(changeSoundEffectVolumeRef, this.transform.position);
+            canPlaySound = false;
+        }
+        else
+            StartCoroutine(PlaySoundEffectSoundCorotine(0.5f));
     }
+
+    private IEnumerator PlaySoundEffectSoundCorotine(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        canPlaySound = true;
+    }
+
 }
