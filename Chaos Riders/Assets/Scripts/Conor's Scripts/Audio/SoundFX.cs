c66cc2420bc02dd235f695d6f3cdd065223e01ad
Tapley123 <conortapley1@gmail.com>
2021-04-10 15:35:49 +0100

    Button Sound on everything

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Audio/SoundFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFX : MonoBehaviour
+{
+    public string buttonSoundRef = "event:/SoundFX/ButtonSound";
+    public string changeSoundEffectVolumeRef = "event:/SoundFX/SoundEffectSound";
+
+    public void PlayButtonSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(buttonSoundRef, this.transform.position);
+    }
+
+    public void PlayChangeSoundEffectSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(changeSoundEffectVolumeRef, this.transform.position);
+    }
+}
