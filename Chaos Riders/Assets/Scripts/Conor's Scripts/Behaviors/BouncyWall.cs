d5131c78042f654fbe9f25021e9ac390b2c6f518
Tapley123 <conortapley1@gmail.com>
2021-04-16 13:30:03 +0100

    Bouncy working

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Behaviors/BouncyWall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyWall : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private Vector3 lastVelocity;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bouncy")
+        {
+            var speed = lastVelocity.magnitude;
+            var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+
+            rb.velocity = direction * Mathf.Max(speed, 0f);
+        }
+    }
+}
