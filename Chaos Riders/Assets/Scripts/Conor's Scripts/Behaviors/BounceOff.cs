45dabed48102f815358a505eac173119c4803f91
Tapley123 <conortapley1@gmail.com>
2021-04-18 15:51:03 +0100

    New Bounce Off script

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Behaviors/BounceOff.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Behaviors/BounceOff.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BouncyWall : MonoBehaviour
+public class BounceOff : MonoBehaviour
 {
     private Rigidbody rb;
 
     private Vector3 lastVelocity;
 
-    [Range (0, 100)]
+    [Range(0, 100)]
     public float wallBouncePercentage = 60f; //the percentage of your speed that you bounce with
 
     [Range(0, 200)]
-    private float barrelBouncePercentage = 150f; //the percentage of your speed that you bounce with
+    public float barrelBouncePercentage = 100f; //the percentage of your speed that you bounce with
 
     void Awake()
     {
@@ -27,7 +27,7 @@ public class BouncyWall : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Bouncy")
+        if (collision.gameObject.tag == "Bouncy")
         {
             var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
             var speed = lastVelocity.magnitude;
@@ -37,7 +37,7 @@ public class BouncyWall : MonoBehaviour
         }
 
 
-        if(collision.gameObject.tag == "Explosive Barrel")
+        if (collision.gameObject.tag == "Explosive Barrel")
         {
             //Debug.Log("You Hit an exlosive barrel");
 
