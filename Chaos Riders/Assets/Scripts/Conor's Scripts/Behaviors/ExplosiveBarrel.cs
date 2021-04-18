e2514df8cc457752ac4d9dc604f01abbb7cccb84
Tapley123 <conortapley1@gmail.com>
2021-04-18 14:21:38 +0100

    Explosive barrel effect

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Behaviors/ExplosiveBarrel.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Behaviors/ExplosiveBarrel.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using System.IO;
 
 public class ExplosiveBarrel : MonoBehaviour
 {
+    private PhotonView pv;
+
     private MeshRenderer[] meshRenderers;
     private Collider[] colliders;
     [SerializeField] private float explodedForTime = 7f;
 
     [SerializeField] private float explosiveDamage = 60f;
     public static float ExplosiveDamage; //used in the Target script to take health off
+
+    [SerializeField] private GameObject explosionEffect;
     
 
     void Awake()
     {
+        pv = GetComponent<PhotonView>();
         meshRenderers = this.GetComponentsInChildren<MeshRenderer>();
         colliders = this.GetComponentsInChildren<Collider>();
         ExplosiveDamage = explosiveDamage;
     }
 
-    void DisableBarrel()
+    void ExplodeBarrel()
     {
         //make invisible
         foreach (MeshRenderer mr in meshRenderers)
@@ -28,9 +35,19 @@ public class ExplosiveBarrel : MonoBehaviour
         //disable the colliders
         foreach (Collider col in colliders)
             col.enabled = false;
+
+        //spawn explosion
+        if (IsThisMultiplayer.Instance.multiplayer)
+        {
+            pv.RPC("Explode", RpcTarget.All);
+        }
+        else
+        {
+            OfflineExplode();
+        }
     }
 
-    void EnableBarrel()
+    void ResetBarrel()
     {
         //make visable
         foreach (MeshRenderer mr in meshRenderers)
@@ -41,21 +58,33 @@ public class ExplosiveBarrel : MonoBehaviour
             col.enabled = true;
     }
 
+    [PunRPC]
+    void Explode()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", explosionEffect.gameObject.name), transform.position, transform.rotation, 0);
+    }
+
+    void OfflineExplode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+    }
+
+
 
 
     private void OnCollisionEnter(Collision collision)
     {
         StartCoroutine(ExplodeCoroutine(explodedForTime));
 
-        collision.gameObject.GetComponent<Target>().health -= explosiveDamage;
+        //collision.gameObject.GetComponent<Target>().health -= explosiveDamage;
     }
 
     private IEnumerator ExplodeCoroutine(float time)
     {
-        DisableBarrel();
+        ExplodeBarrel();
 
         yield return new WaitForSeconds(time);
 
-        EnableBarrel();
+        ResetBarrel();
     }
 }
