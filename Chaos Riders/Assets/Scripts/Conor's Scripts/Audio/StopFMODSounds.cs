ed8bf107df0198564eafef48791a84e5c54e8026
Tapley123 <conortapley1@gmail.com>
2021-04-21 01:03:06 +0100

    All FMOD sounds stop playing

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Audio/StopFMODSounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+using FMODUnity;
+
+public class StopFMODSounds : MonoBehaviour
+{
+    private FMOD.Studio.Bus MasterBus;
+
+    private void Start()
+    {
+        MasterBus = FMODUnity.RuntimeManager.GetBus("Bus:/");
+    }
+
+    public void StopAllSounds()
+    {
+        MasterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
