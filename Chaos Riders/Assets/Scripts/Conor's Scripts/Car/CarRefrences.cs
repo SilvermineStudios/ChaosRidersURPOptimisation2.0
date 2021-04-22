9363fcd77e1be4d5f3a93294bcd8dd1a9c8af2a0
Tapley123 <conortapley1@gmail.com>
2021-04-22 11:35:39 +0100

    Follow Braker Working Needs Reference

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Car/CarRefrences.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarRefrences : MonoBehaviour
+{
+    public Transform FL;
+    public Transform FR;
+}
---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Car/SmoothFollowCar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollowCar : MonoBehaviour
+{
+    [Header("Car Stuff")]
+    public GameObject Target;
+    private Transform TargetCar;
+    
+    [Header("My Wheels")]
+    public Transform MyFL;
+    public Transform MyFR;
+
+    //Wheels to follow
+    private Transform FL;
+    private Transform FR;
+
+    //public float heightDamping = 3;
+    //public float rotationDamping = 3;
+
+    private void Awake()
+    {
+        TargetCar = Target.transform;
+
+        FL = Target.GetComponent<CarRefrences>().FL;
+        FR = Target.GetComponent<CarRefrences>().FR;
+    }
+
+    void LateUpdate()
+    {
+        //Move + Rotate Car 
+        transform.position = Vector3.Lerp(transform.position, TargetCar.position, Time.deltaTime * 100);
+        transform.rotation = Quaternion.Lerp(transform.rotation, TargetCar.rotation, Time.deltaTime * 100);
+
+        //Rotate Wheels
+        MyFL.rotation = Quaternion.Lerp(FL.rotation, FL.rotation, Time.deltaTime * 100);
+        MyFR.rotation = Quaternion.Lerp(FR.rotation, FR.rotation, Time.deltaTime * 100);
+    }
+}
