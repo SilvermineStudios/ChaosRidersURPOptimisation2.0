bd0fa4190de16fecfec5fb6d4c505d46a3c6d4d1
Tapley123 <conortapley1@gmail.com>
2021-04-23 14:11:15 +0100

    map stuff

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/TrapManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapManager : MonoBehaviour
+{
+    [Header ("Trap Damage")]
+    [SerializeField] private float explosiveBarrelDamage = 60f;
+    [SerializeField] private float bladeTrapDamage = 6f;
+
+    //public statics
+    public static float ExplosiveBarrelDamage;
+    public static float BladeTrapDamage;
+
+    void Awake()
+    {
+        ExplosiveBarrelDamage = explosiveBarrelDamage;
+        BladeTrapDamage = bladeTrapDamage;
+    }
+}
