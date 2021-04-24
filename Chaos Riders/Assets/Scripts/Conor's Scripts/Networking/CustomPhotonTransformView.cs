20a56e7d8e6afef1f04821fec6b4431aa68b6e57
Tapley123 <conortapley1@gmail.com>
2021-04-24 19:24:32 +0100

    FINALLY car doesnt look laggy for shooter

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Networking/CustomPhotonTransformView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+
+public class CustomPhotonTransformView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.SendRate = 40; //usually 20
+        PhotonNetwork.SerializationRate = 30; //usually 10
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
