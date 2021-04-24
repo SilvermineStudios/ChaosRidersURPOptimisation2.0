6e5e2e2b6e075fda9753e8397e24ca8a18aac2ca
Tapley123 <conortapley1@gmail.com>
2021-04-25 00:31:30 +0100

    controls panels

---
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Behaviors/TurnOffWithButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffWithButton : MonoBehaviour
+{
+    public KeyCode buttonToPress = KeyCode.X;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(buttonToPress))
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
