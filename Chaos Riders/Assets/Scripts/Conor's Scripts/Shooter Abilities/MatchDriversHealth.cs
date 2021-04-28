c6f07b2b7975126de51964faed5147098c8d0f81
Tapley123 <conortapley1@gmail.com>
2021-04-28 14:28:02 +0100

    Matching driver health

--- Chaos Riders/Assets/Scripts/Conor's Scripts/Shooter Abilities/MatchDriversHealth.cs
+++ Chaos Riders/Assets/Scripts/Conor's Scripts/Shooter Abilities/MatchDriversHealth.cs
@@ -6,13 +6,9 @@ using Photon.Pun;
 public class MatchDriversHealth : MonoBehaviour
 {
     private Shooter ShooterScript; //ref to the shooter script so you can access the car gameobject
+    [SerializeField] private Target CarHealth; //ref to the shooter script so you can access the car gameobject
     private GameObject car; //ref to the connected car
-    [SerializeField] private GameObject healthBarAboveCar;
     [SerializeField] private Transform myHealthBarUi;
-    private Transform carHealthBarUI;
-
-    [SerializeField] private float health, startHealth, healthNormalized;
-    [SerializeField] private bool setStartHealth = false;
 
     private PhotonView pv;
 
@@ -23,12 +19,6 @@ public class MatchDriversHealth : MonoBehaviour
         ShooterScript = GetComponent<Shooter>();
     }
 
-    void Start()
-    {
-        
-    }
-
-
     void Update()
     {
         if (pv.IsMine && IsThisMultiplayer.Instance.multiplayer || !IsThisMultiplayer.Instance.multiplayer)
@@ -38,43 +28,18 @@ public class MatchDriversHealth : MonoBehaviour
             {
                 car = ShooterScript.car;
 
-                if(car.tag == "car")
-                {
-                    //healthBarAboveCar = car.GetComponent<Health>().myHealthBar;
-                    //carHealthBarUI = car.GetComponent<Health>().healthBarUi;
-                    healthBarAboveCar = car.GetComponent<Target>().myHealthBar;
-                    carHealthBarUI = car.GetComponent<Target>().healthBarUi;
-                }
-                if(car.tag != "car")
-                {
-                    //healthBarAboveCar = car.GetComponent<AIHealth>().myHealthBar;
-                    //health = car.GetComponent<AIHealth>().health;
-                    //startHealth = car.GetComponent<AIHealth>().startHealth;
-                    //healthNormalized = car.GetComponent<AIHealth>().healthNormalized;
+                if (CarHealth == null)
+                    CarHealth = car.GetComponent<Target>();
 
-                    healthBarAboveCar = car.GetComponent<Target>().myHealthBar;
-                    health = car.GetComponent<Target>().health;
-                    startHealth = car.GetComponent<Target>().startHealth;
-                    healthNormalized = car.GetComponent<Target>().healthNormalized;
-
-                    SetHealthBarUiSize(healthNormalized);
-                }
-
-                if (healthBarAboveCar != null)
-                {
-                    healthBarAboveCar.SetActive(false); //make the cars healthbar invisable to the gunner
-
-
-                    //myHealthBarUi.localScale = carHealthBarUI.localScale;
-                    //myHealthBarUi.localScale = new Vector3(1f, sizeNormalized);
-                }
-
+                CarHealth.myHealthBar.SetActive(false); //make the cars healthbar invisable to the gunner
+                SetHealthBarUiSize(CarHealth.healthNormalized);
             }
         }
     }
 
     private void SetHealthBarUiSize(float sizeNormalized)
     {
+        Debug.Log("Health Normalized is: " + CarHealth.healthNormalized);
         myHealthBarUi.localScale = new Vector3(1f, sizeNormalized);
     }
 }
