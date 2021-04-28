420b1fbd63cd0b17a8890b08d4bf745183a6ac4d
Oisin <C17343216@mydit.ie>
2021-04-28 13:57:07 +0100

    s

---
+++ Chaos Riders/Assets/Scripts/Oisins Scripts/DisplayControlls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayControlls : MonoBehaviour
+{
+    [SerializeField] GameObject[] Keyboard;
+    [SerializeField] GameObject[] Controller;
+    string[] Controllers;
+   
+    
+    void Update()
+    {
+        Controllers = Input.GetJoystickNames();
+
+        if(Controllers == null)
+        {
+            foreach (GameObject g in Keyboard)
+            {
+                g.SetActive(true);
+            }
+            foreach (GameObject g in Controller)
+            {
+                g.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (GameObject g in Controller)
+            {
+                g.SetActive(true);
+            }
+            foreach (GameObject g in Keyboard)
+            {
+                g.SetActive(false);
+            }
+        }
+    }
+}
