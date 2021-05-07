774478683d490f2cac160d6a2d49f3d5fc464d29
Tapley123 <conortapley1@gmail.com>
2021-05-07 01:11:48 +0100

    RBG explosion better

--- Chaos Riders/Assets/Scripts/Oisins Scripts/Guns/RPG.cs
+++ Chaos Riders/Assets/Scripts/Oisins Scripts/Guns/RPG.cs
@@ -6,22 +6,90 @@ using System.IO;
 
 public class RPG : MonoBehaviour
 {
-    [SerializeField] GameObject explosionEffect;
+    //[SerializeField] GameObject explosionEffect;
+    [SerializeField] GameObject trailSmokeVFX;
+    [SerializeField] GameObject explosionVFX;
     [SerializeField] float damage;
     [SerializeField] float radius;
-    [SerializeField] PhotonView pv;
+    [SerializeField] float timeUntilDeleted = 3f;
+
+    private Rigidbody rb;
+    private PhotonView pv;
+
+    private void Awake()
+    {
+        pv = this.GetComponent<PhotonView>();
+        rb = this.GetComponent<Rigidbody>();
+
+        trailSmokeVFX.SetActive(true);
+        explosionVFX.SetActive(false);
+    }
+
+    //new
+    void ExplosiveDamage()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearby in colliders)
+        {
+            GameObject objectHit = nearby.transform.root.gameObject;
+            Target target = objectHit.GetComponent<Target>();
+
+            if (target != null && !target.hitByRPG)
+            {
+                target.hitByRPG = true;
+                target.TakeDamage(damage);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+        ExplosiveDamage();
+        explosionVFX.SetActive(true);
+
+        ExplosionCoroutine(timeUntilDeleted);
+        
+
+        /*
+        if(IsThisMultiplayer.Instance.multiplayer && pv.IsMine)
+        {
+            pv.RPC("Explode", RpcTarget.All);
+        }
+        else
+        {
+            OfflineExplode();
+        }
+        */
+    }
+
+    private IEnumerator ExplosionCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        explosionVFX.SetActive(false);
+        PhotonNetwork.Destroy(this.gameObject);
+    }
 
 
+
+
+
+
+
+    //Old Shit
     void OfflineExplode()
     {
-        Instantiate(explosionEffect, transform.position, transform.rotation);
+        //Instantiate(explosionEffect, transform.position, transform.rotation);
 
-        Collider[] colliders =  Physics.OverlapSphere(transform.position, radius);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
 
-        foreach(Collider nearby in colliders)
+        foreach (Collider nearby in colliders)
         {
             Target health = nearby.GetComponent<Target>();
-            if(health != null)
+            if (health != null)
             {
                 health.TakeDamage(damage);
             }
@@ -38,23 +106,10 @@ public class RPG : MonoBehaviour
         Destroy(gameObject);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(IsThisMultiplayer.Instance.multiplayer && pv.IsMine)
-        {
-            pv.RPC("Explode", RpcTarget.All);
-        }
-        else
-        {
-            OfflineExplode();
-        }
-        
-    }
-
     [PunRPC]
     void Explode()
     {
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "explosionEffect"), transform.position, transform.rotation, 0);
+        //PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "explosionEffect"), transform.position, transform.rotation, 0);
 
         Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
 
@@ -74,8 +129,6 @@ public class RPG : MonoBehaviour
             }
             */
         }
-
         Destroy(gameObject);
     }
-
 }
