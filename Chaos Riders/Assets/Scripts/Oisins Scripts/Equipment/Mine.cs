cf1dea99548db725f77c185dca8678ca86da540c
Tapley123 <conortapley1@gmail.com>
2021-05-07 02:43:44 +0100

    Mine Effects

--- Chaos Riders/Assets/Scripts/Oisins Scripts/Equipment/Mine.cs
+++ Chaos Riders/Assets/Scripts/Oisins Scripts/Equipment/Mine.cs
@@ -6,11 +6,18 @@ using System.IO;
 
 public class Mine : MonoBehaviour
 {
-    [SerializeField] GameObject explosionEffect;
+    //[SerializeField] GameObject explosionEffect;
+    [SerializeField] GameObject explosionEffectVFX;
     [SerializeField] float damage;
     [SerializeField] float radius;
     [SerializeField] PhotonView pv;
     [SerializeField] float waitTime;
+    [SerializeField] float timeUntilDeleted = 5f;
+
+    private void Awake()
+    {
+        explosionEffectVFX.SetActive(false);
+    }
 
     private void FixedUpdate()
     {
@@ -20,9 +27,58 @@ public class Mine : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (waitTime <= 0)
+        {
+            StartCoroutine(ExplodeCoroutine(timeUntilDeleted));
+
+
+            /*
+            if (!IsThisMultiplayer.Instance.multiplayer)
+            {
+                //OfflineExplode();
+            }
+            else
+            {
+                //pv.RPC("Explode", RpcTarget.All);
+            }
+            */
+        }
+    }
+
+    private IEnumerator ExplodeCoroutine(float time)
+    {
+        explosionEffectVFX.SetActive(true);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearby in colliders)
+        {
+            GameObject objectHit = nearby.transform.root.gameObject;
+            Target target = objectHit.GetComponent<Target>();
+
+            if (target != null && !target.hitByMine)
+            {
+                target.hitByMine = true;
+                target.TakeDamage(damage);
+                Debug.Log("You hit " + objectHit.name + " which has a target script attached");
+            }
+        }
+
+        yield return new WaitForSeconds(time);
+
+        if (IsThisMultiplayer.Instance.multiplayer)
+            PhotonNetwork.Destroy(this.gameObject);
+        else
+            Destroy(this.gameObject);
+    }
+
+    /*
     void OfflineExplode()
     {
         Instantiate(explosionEffect, transform.position, transform.rotation);
+
         Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
 
         foreach (Collider nearby in colliders)
@@ -44,6 +100,7 @@ public class Mine : MonoBehaviour
     void Explode()
     {
         PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "explosionEffect"), transform.position, transform.rotation, 0);
+
         Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
 
         foreach (Collider nearby in colliders)
@@ -60,16 +117,5 @@ public class Mine : MonoBehaviour
         }
         Destroy(gameObject);
     }
-
-
-    private void OnTriggerEnter(Collider collision)
-    {
-        if (waitTime <= 0)
-        {
-            if (!IsThisMultiplayer.Instance.multiplayer)
-                OfflineExplode();
-            else
-                pv.RPC("Explode", RpcTarget.All);
-        }
-    }
+    */
 }
