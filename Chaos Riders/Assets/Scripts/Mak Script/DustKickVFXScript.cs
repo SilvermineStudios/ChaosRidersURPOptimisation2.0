c2523c8c0713951e953a0d93681b14c7fa74036b
Oisin <C17343216@mydit.ie>
2021-05-08 19:57:52 +0100

    Update DustKickVFXScript.cs

--- Chaos Riders/Assets/Scripts/Mak Script/DustKickVFXScript.cs
+++ Chaos Riders/Assets/Scripts/Mak Script/DustKickVFXScript.cs
@@ -14,11 +14,13 @@ public class DustKickVFXScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //get original components (front particle use same number and baack particles use same, meaning back and front have diff values)
-        BackSpawnRate = BackLeftDustKickUp.GetInt("Spawn Rate");
+         //get original components (front particle use same number and baack particles use same, meaning back and front have diff values)
+         BackSpawnRate = BackLeftDustKickUp.GetInt("Spawn Rate");
          FrontSpawnRate = FrontLeftDustKickUp.GetInt("Spawn Rate");
+
          FrontMinVel = FrontLeftDustKickUp.GetVector3("MinVelocity");
          FrontMaxVel = FrontLeftDustKickUp.GetVector3("MaxVelocity");
+
          BackMinVel = FrontLeftDustKickUp.GetVector3("MinVelocity");
          BackMaxVel = FrontLeftDustKickUp.GetVector3("MaxVelocity");
        
@@ -37,30 +39,39 @@ public class DustKickVFXScript : MonoBehaviour
         //edits the spawn rates for the vfx
         BackLeftDustKickUp.SetInt("Spawn Rate", Mathf.RoundToInt(Mathf.Lerp(0, BackSpawnRate, LerpPercentage)));
         BackRightDustKickUp.SetInt("Spawn Rate", Mathf.RoundToInt(Mathf.Lerp(0, BackSpawnRate, LerpPercentage)));
+
         FrontLeftDustKickUp.SetInt("Spawn Rate", Mathf.RoundToInt(Mathf.Lerp(0, FrontSpawnRate, LerpPercentage)));
         FrontRightDustKickUp.SetInt("Spawn Rate", Mathf.RoundToInt(Mathf.Lerp(0, FrontSpawnRate, LerpPercentage)));
         
         //Calculates the value after lerping
         TestBackMinVel.x = Mathf.Lerp(0, BackMinVel.x, LerpPercentage);
         TestBackMinVel.y = Mathf.Lerp(0, BackMinVel.y, LerpPercentage);
+
         TestBackMinVel.z = Mathf.Lerp(0, BackMinVel.z, LerpPercentage);
         TestBackMaxVel.x = Mathf.Lerp(0, BackMaxVel.x, LerpPercentage);
+
         TestBackMaxVel.y = Mathf.Lerp(0, BackMaxVel.y, LerpPercentage);
         TestBackMaxVel.z = Mathf.Lerp(0, BackMaxVel.z, LerpPercentage);
+
         TestFrontMinVel.x = Mathf.Lerp(0, FrontMinVel.x, LerpPercentage);
         TestFrontMinVel.y = Mathf.Lerp(0, FrontMinVel.y, LerpPercentage);
+
         TestFrontMinVel.z = Mathf.Lerp(0, FrontMinVel.z, LerpPercentage);
         TestFrontMaxVel.x = Mathf.Lerp(0, FrontMaxVel.x, LerpPercentage);
+
         TestFrontMaxVel.y = Mathf.Lerp(0, FrontMaxVel.y, LerpPercentage);
         TestFrontMaxVel.z = Mathf.Lerp(0, FrontMaxVel.z, LerpPercentage);
        
         //assigns the lerped vector3 to VFX
         BackLeftDustKickUp.SetVector3("MinVelocity", TestBackMinVel);
         BackLeftDustKickUp.SetVector3("MaxVelocity", TestBackMaxVel);
+
         BackRightDustKickUp.SetVector3("MinVelocity", TestBackMinVel);
         BackRightDustKickUp.SetVector3("MaxVelocity", TestBackMaxVel);
+
         FrontLeftDustKickUp.SetVector3("MinVelocity", TestFrontMinVel);
         FrontLeftDustKickUp.SetVector3("MaxVelocity", TestFrontMaxVel);
+
         FrontRightDustKickUp.SetVector3("MinVelocity", TestFrontMinVel);
         FrontRightDustKickUp.SetVector3("MaxVelocity", TestFrontMaxVel); 
     }
